ab9967a20ff1428d32919328b009bed4519f4d4c
Gevitiso <aidenlegault@outlook.com>
2024-01-27 19:22:18 -0400

    Change
    asd

--- Minigames/Dog/AidenDog/ChasedPosition.gd
+++ Minigames/Dog/AidenDog/ChasedPosition.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 #____ being chased by dog
 
-func location():
-player = get_node("../Player")
-	var direction = (player.position - self.position).normalized()
+#func location():
+#player = get_node("../Player")
+	#var direction = (player.position - self.position).normalized()
